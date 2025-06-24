64ca22d400330dc0049dd142ec981006c26e067e
Ben <ben.r.fowler@hotmail.com>
2025-06-24 13:54:39 +1000

    adding receiving mode which disables ability to record

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,6 +4,8 @@ public class GameController : MonoBehaviour
 {
     public static GameController Instance { get; private set; }
 
+    private bool isReceiving = false;
+
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -16,15 +18,45 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (isReceiving)
+            {
+                StopReceiving();
+            }
+            else
+            {
+                StartReceiving();
+            }
+
+        }
+
+        if (isReceiving)
+            return; // Block input if receiving
+
         if (Input.GetKeyDown(KeyCode.T))
         {
             UIController.Instance.ShowHand(UIController.HandName.Transmitting);
         }
 
-        // On release, clear the hand
         if (Input.GetKeyUp(KeyCode.T))
         {
             UIController.Instance.ShowHand(UIController.HandName.Idle);
         }
+
+
+    }
+
+    public void StartReceiving()
+    {
+        isReceiving = true;
+        UIController.Instance.ShowHand(UIController.HandName.Receiving);
+    }
+
+    public void StopReceiving()
+    {
+        isReceiving = false;
+        UIController.Instance.ShowHand(UIController.HandName.Idle);
     }
 }
