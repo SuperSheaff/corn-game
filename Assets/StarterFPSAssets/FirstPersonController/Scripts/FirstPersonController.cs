1298560742cc3022546ba24a6cc4621c50a80351
Oscar J <oscarjemmott@gmail.com>
2025-06-27 08:13:49 +1000

    fix jump bug when !MovementAllowed

--- Assets/StarterFPSAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterFPSAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -272,7 +272,7 @@ namespace StarterAssets
 				if (_input.jump && _jumpTimeoutDelta <= 0.0f)
 				{
 					// the square root of H * -2 * G = how much velocity needed to reach desired height
-					_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+					if(MovementAllowed)_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
 					//fmod
 					if (!jumped)
 					{
